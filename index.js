57953008d1f63d0ce0631f3fd22ade2a5f21f1c8
Hoa Le <Hoa_le9485@yahoo.com>
2021-11-04 17:55:18 -0400

    commit tree b6c73e8cb1f16fab71f81eaac79b0d8d1aa82a46

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+// filter and return any name that matches the 2nd parameter (string)
+function findMatching(listOfDriver, string) {
+    return listOfDriver.filter(possibleMatch =>
+        possibleMatch.toLowerCase() === string.toLowerCase()
+        )
+}
+
+// filter out and return match that begin with provide letter.
+function fuzzyMatch(name, letterString) {
+    return name.filter( possibleMatch => 
+        possibleMatch.toLowerCase().indexOf(letterString.toLowerCase()) === 0
+        // make it to lower case and match only the first index [0] which is the first char.
+        )
+}
+
+function matchName(name, soughtName) {
+    return name.filter( possibleMatch => possibleMatch.name === soughtName)
+  }
+
+
+// string as argu // return all driver first letter match with provided letter
